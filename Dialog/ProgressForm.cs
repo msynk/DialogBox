9d2f85ac2a58f15efc1adde495135ad43f3006e4
ArsenShnurkov <arsen.shnurkov@gmail.com>
2015-06-01 13:23:45 +0300

    BackgroundWorker sample was added

---
+++ Dialog/ProgressForm.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Dialog
+{
+    public partial class ProgressForm : Form
+    {
+	public static DialogBoxResult CreateAndShowForm()
+	{
+	    var form = new ProgressForm ();
+	    form.ShowDialog();
+	    if (form.DialogResult == DialogResult.OK) {
+		return DialogBoxResult.Button1;
+	    } else {
+		return DialogBoxResult.Button2;
+	    }
+	}
+
+	public ProgressForm ()
+	{
+	    InitializeComponent ();
+	    backgroundWorker1 = new BackgroundWorker();
+	    backgroundWorker1.DoWork += this.backgroundWorker1_DoWork;
+	    backgroundWorker1.WorkerReportsProgress = true;
+	    backgroundWorker1.WorkerSupportsCancellation = true;
+	    backgroundWorker1.ProgressChanged += this.backgroundWorker1_OnProgressChanged;
+	    backgroundWorker1.RunWorkerCompleted += this.backgroundWorker1_OnRunWorkerCompleted;
+	    backgroundWorker1.RunWorkerAsync();
+	}
+
+	private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+	{
+	    BackgroundWorker worker = sender as BackgroundWorker;
+
+	    for (int i = 1; i <= 50; i++)
+	    {
+		if (worker.CancellationPending == true)
+		{
+		    e.Cancel = true;
+		    Trace.WriteLine ("Нить остановлена");
+		    break;
+		}
+		else
+		{
+		    // Perform a time consuming operation and report progress.
+		    System.Threading.Thread.Sleep(500);
+		    worker.ReportProgress(i * 200);
+		}
+	    }
+	    Trace.WriteLine ("Нить отработала");
+	}
+
+	void backgroundWorker1_OnProgressChanged(object sender, ProgressChangedEventArgs e)
+	{
+	    this.progressBar1.Value = e.ProgressPercentage/100;
+	}
+
+	void backgroundWorker1_OnRunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+	{
+	    this.Close ();
+	}
+
+	private void OnFormClosing_Handler(object sender, System.Windows.Forms.FormClosingEventArgs e)
+	{
+	    backgroundWorker1.CancelAsync();
+	}
+
+	private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
--- TestDialogBox/Form1.cs
+++ TestDialogBox/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Dialog;
 
 namespace TestDialogBox
@@ -9,11 +11,66 @@ namespace TestDialogBox
     public Form1()
     {
       InitializeComponent();
+	    SetupButtonsPositions ();
     }
+	private void SetupButtonsPositions()
+	{
+	    var buttons = new List<Button>();
+	    int totalwidth = 0;
+	    int totalheight = 0;
+	    foreach (var ctl in this.Controls)
+	    {
+		if (ctl is Button)
+		{
+		    var b = (Button)ctl;
+		    buttons.Add(b);
+		    totalwidth += b.Size.Width;
+		    totalheight += b.Size.Height;
+		}
+	    }
+	    int count = buttons.Count;
+	    var spacingx = (this.ClientSize.Width - totalwidth) / (count + 2);
+	    var spacingy = (this.ClientSize.Height - totalheight) / (count + 2);
+	    int runningWidth = 0;
+	    int runningHeight = 0;
+	    for (int pos = 0; pos < buttons.Count; ++pos)
+	    {
+		int posx = spacingx /*+ runningWidth + pos * spacingx*/;
+		int posy = spacingy + runningHeight + pos * spacingy;
+		buttons[pos].Location = new System.Drawing.Point(posx, posy);
+		runningWidth += buttons[pos].Size.Width;
+		runningHeight += buttons[pos].Size.Height;
+	    }
+	}
+	private void OnSizeChanged_Handler(object sender, EventArgs e)
+	{
+	    SetupButtonsPositions ();
+	}
+	private void btnShowProgressForm_Click(object sender, EventArgs e)
+	{
+	    var result = ProgressForm.CreateAndShowForm();
+	    if (result == DialogBoxResult.Button1) {
+		Trace.WriteLine ("Процесс был успешно выполнен");
+	    } else {
+		Trace.WriteLine ("Процесс был принудительно завершен");
+	    }
+	}
+	private void btnShowProgressFormModal_Click(object sender, EventArgs e)
+	{
+	    // Start the BackgroundWorker.
+	    var result = ProgressForm.CreateAndShowForm();
+	    if (result == DialogBoxResult.Button1) {
+		Trace.WriteLine ("Процесс был успешно выполнен");
+	    } else {
+		Trace.WriteLine ("Процесс был принудительно завершен");
+	    }
+	}
+
+
 
     private void btnShowMessage_Click(object sender, EventArgs e)
     {
-      var result = DialogBox.Show("این یک خطاست. جان اصغر!", "هشدار خطا", MessageBoxIcon.Asterisk, MessageBoxButtons.AbortRetryIgnore, "اینها همگی جزئیات خطا هستند.");
+      /*var result =*/ DialogBox.Show("این یک خطاست. جان اصغر!", "هشدار خطا", MessageBoxIcon.Asterisk, MessageBoxButtons.AbortRetryIgnore, "اینها همگی جزئیات خطا هستند.");
     }
 
     private void btnWait_Click(object sender, EventArgs e)
